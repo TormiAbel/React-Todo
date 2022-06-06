b322fbcf03c995e997ac2d6535aa6161e88ea40b
TormiAbel <73690456+TormiAbel@users.noreply.github.com>
2022-06-06 12:27:55 +0300

    Update Login.js

--- src/Login.js
+++ src/Login.js
@@ -4,7 +4,6 @@ import React, { useState } from "react";
 export default function Login() {
   let [username, setUsername] = useState('');
   let [password, setPassword] = useState('');
-  const secretKey = "pass";
 
   const userNN = (event) => {
     setUsername(event.target.value);
