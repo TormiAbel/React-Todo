a5caa09b40da18a3a541f66420ac32f09f561f4d
TormiAbel <73690456+TormiAbel@users.noreply.github.com>
2022-06-06 12:29:13 +0300

    Update TaskView.js

--- src/TaskView.js
+++ src/TaskView.js
@@ -1,34 +1,121 @@
-import logo from './logo.svg';
 import './App.css';
 import React, { useState } from 'react';
 
-export default function TaskView() {
+export default function TaskView({user, setUser}) {
   let [taskName, setTaskName] = useState("");
-  let [taskList, setTaskList] = useState(["t1", "t2"]);
+  let [tasks, setTasks] = useState([]);
+  let [checked, setChecked] = useState();
 
 
-  const handleInputName = (event) => {
-    setTaskName(event.target.value)
+  var extraGetInfo = {
+    method: "GET",
+    Credential: "include",
+    headers: {
+      "content-type": "application/json",
+      Authorization: "Bearer " + user.access_token,
+    },
+  };
+  var sendTaskToApi = {  
+    method: 'POST',
+    Credential: "include",
+    headers: {
+      "content-type": "application/json",
+      Authorization: "Bearer " + user.access_token,
+    },
+   body: JSON.stringify({ 
+     "title": taskName,
+     "desc": ""
+  })
   }
+  var sendDelete = {  
+    method: 'DELETE',
+    Credential: "include",
+    headers: {
+      "content-type": "application/json",
+      Authorization: "Bearer " + user.access_token,
+    },
+   body: JSON.stringify({ 
+     "title": taskName,
+     "desc": ""
+  })
+  }
+  var updateInfo = {  
+    method: 'PUT',
+    Credential: "include",
+    headers: {
+      "content-type": "application/json",
+      Authorization: "Bearer " + user.access_token,
+    },
+   body: JSON.stringify({ 
+     "marked_as_done": checked
+  })
+  }
+
+  
+
+  useEffect(() => {fetch("http://demo2.z-bit.ee/tasks", extraGetInfo)
+  .then((res) => res.json())
+  .then((result) => {
+    setTasks(result);
+  })});
+
+  
+  const eventHandler = (event) => {
+    setTaskName(event.target.value);
+  };
+  const updateStatus = (task) => {
+    console.log(task.marked_as_done)
+    setChecked(!task.marked_as_done);
+    console.log("checked", checked)
+    let id =(task.id)
+    fetch("http://demo2.z-bit.ee/tasks/"+id, updateInfo)
+      .then((res) => res.json())
+      .then((result) => {
+        console.log("tasks", tasks)
+        setTasks(tasks);
+      });
+  };
 
   const addTask = () => {
-    setTaskList([...taskList, taskName])
-  }
+    console.log(taskName)
+    fetch("http://demo2.z-bit.ee/tasks", sendTaskToApi)
+      .then((res) => res.json())
+      .then((result) => {
+        let copy = [...tasks, result];
+        console.log("copy", copy)
+        setTasks(copy);
+      });
+  };
 
-  const deleteTask = (index) => {
-    let copy = [...taskList]
-    copy.splice(index, 1)
-    setTaskList(copy)
-  }
+  const deleteTask = (task) => {
+    let id = (task.id)
+    fetch("http://demo2.z-bit.ee/tasks/"+id, sendDelete)
+      .then((res) => res.json())
+      .then((result) => {
+        let copy = [...tasks];
+        copy.splice(task, 1);
+        setTasks(copy);
+      });
+  };
   return (
     <div className="App">
       <header className="App-header">
-        <input onChange={handleInputName}/>
-        <button onClick={addTask}>Add Task</button>
-        <br/>
+        <input onChange={eventHandler} />
+        <button onClick={addTask}> Add task</button>
+        <br />
+        Sul on vaja teha veel asjad:
         <ul>
-          {taskList.map((task, index) => (
-          <li>{index}. {task} <button onClick={() => deleteTask(index)}>Delete Task</button> </li>
+          {tasks.map((task, index) => (
+            <li>
+              <input
+          type="checkbox"
+          checked={task.marked_as_done}
+          onChange={() => updateStatus(task)}
+        />
+              {" "}
+              {index}. {task.title}{" "}
+              <button onClick={() => deleteTask(task)}>Delete</button>
+            </li>
           ))}
         </ul>
       </header>
