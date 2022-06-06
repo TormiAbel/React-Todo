22736ba3f1f50da96c3dfd3271fb03b0a9f7e518
TormiAbel <73690456+TormiAbel@users.noreply.github.com>
2022-06-06 12:25:14 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,13 +5,20 @@ import Login from './Login.js';
 
 function App() {
   let [user, setUser] = useState(null);
-  
+  const greetRndm = <p>Logi Sisse!</p>
+  const greetUser = <p>Tere {user?.username}</p>
   
   return (
     <div className="App">
-    
-        {!user && <Login user={user} />}
-        {user && <TaskView />}
+      <header className="App-header">
+        {!user && greetRndm}
+        {user && greetUser}
+
+        {!user && <Login user={user} setUser={setUser}/>}
+        {user && <TaskView user={user} setUser={setUser}/>}
+      </header>
     </div>
   );
 }
+
+export default App;
